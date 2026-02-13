f416d32206a42399fe897d1150e9310c2547e28c
carol-Brapira <carol@brapira.com.br>
2026-02-13 17:56:32 -0300

    add: função de selecionar selectedbox

--- function/funcoes_rotina.py
+++ function/funcoes_rotina.py
@@ -1,6 +1,6 @@
 
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 def desmarcar_item(tipoInput, wait, driver, name, value, descricao, CODIGO_ROTINA):
     item = wait.until(
@@ -23,3 +23,10 @@ def marcar_item(tipoInput, wait, driver, name, value, descricao, CODIGO_ROTINA):
     if not item.is_selected():
         driver.execute_script("arguments[0].click();", item)
         print(f"ROTINA {CODIGO_ROTINA}:⚙️ {tipoInput} de {descricao} marcado...")
+
+def selecionar_selectedbox(wait, driver, name, value, quebra, label, CODIGO_ROTINA):
+    select = wait.until(EC.presence_of_element_located((By.NAME, name)))
+
+    driver.execute_script(f"arguments[0].value = '{value}'; arguments[0].onchange();", select)
+
+    print(f"ROTINA {CODIGO_ROTINA}:⚙️ A quebra {quebra} foi configurada para {label}")
